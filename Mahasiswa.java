c695171411f885768cf9e238e7925471953ce067
Dombby <daffaaramadhan10@gmail.com>
2026-04-12 17:05:29 +0800

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,16 @@
+package com.mycompany.kuliah;
+
+public class Mahasiswa {
+    public static String npm;
+    public static String nama;
+    public static double ipk;
+    public static int semester;
+
+    public static void getMahasiswa() {
+        System.out.println("NPM       : " + npm);
+        System.out.println("Nama      : " + nama);
+        System.out.println("IPK       : " + ipk);
+        System.out.println("Semester  : " + semester);
+    }
+}
+
